0bf8de40337c9a3db4f85f4f176a437f7094e7ab
Allison Fister <allisonfister59@gmail.com>
2024-02-04 14:43:50 -0600

    add trout

---
+++ rainbow-trout.py
@@ -0,0 +1,41 @@
+import pyxel
+
+class App:
+    def __init__(self):
+        pyxel.init(64, 32, title="Rainbow Trout")
+        pyxel.load("rainbow-trout.pyxres")
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q) or \
+           pyxel.btnp(pyxel.KEY_ESCAPE):
+            pyxel.quit()
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.blt(-2, 0, 0, 0, 0, 64, 32)
+        color = None
+        for y in range(32):
+            for x in range(64):
+                if pyxel.pget(x, y) == 7: # body
+                    n = pyxel.noise(x / 10, y / 10, pyxel.frame_count / 45)
+                    if n > 0.6:
+                        color = 8 # red
+                    elif n > 0.3:
+                        color = 9 # orange
+                    elif n > 0:
+                        color = 10 # yellow
+                    elif n > -0.3:
+                        color = 3 # green
+                    elif n > -0.6:
+                        color = 5 # blue
+                    else:
+                        color = 2 # purple
+                    pyxel.pset(x, y, color)
+                elif pyxel.pget(x, y) == 1: # outline
+                    pyxel.pset(x, y, 0)
+                elif pyxel.pget(x, y) == 0: # background
+                    pyxel.pset(x, y, 7)
+        pyxel.pset(10, 14, 7) # eye
+
+App()
